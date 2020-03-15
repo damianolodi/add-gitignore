a55b658be5b1b570cac5fd10685832b2dabd3726
Damiano <damiano.lodi.93@gmail.com>
2020-03-15 15:35:51 +0100

    Create the templates.py file
    This file will contain the templates of all the supported programs.
    Each tempalte is utimately contained in a dicitonary, in which keys
    are the name of the program (all lowercase) and the values are
    strings containing the text that should be added to the gitignore.

---
+++ templates.py
@@ -0,0 +1,20 @@
+main = """# OS generated files #
+######################
+.DS_Store
+.DS_Store?
+._*
+.Spotlight-V100
+.Trashes
+ehthumbs.db
+Thumbs.db
+
+# Miscellanea #
+###############
+__pycache__/
+"""
+
+freecad = """# FreeCAD generated files #
+###########################
+*.FCStd1"""
+
+programs = {"freecad": freecad, "kicad": "None"}
