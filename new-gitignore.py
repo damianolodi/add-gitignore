155de6a3185d8fde605d4fe6adca9330ba5714b9
Damiano <damiano.lodi.93@gmail.com>
2020-03-15 15:40:55 +0100

    Add all working parts of the program to function in a basic way

--- new-gitignore.py
+++ new-gitignore.py
@@ -1,25 +1,64 @@
 import sys
 import os
+import templates
 
 
-def copy_main_template(path):
-    pass
+def check_gitignore_existance(path):
+    file_list = os.listdir(path)
+    if ".gitignore" in file_list:
+        return True
+    return False
+
+
+def create_gitignore(path):
+    with open(path + "/.gitignore", "a") as gitignore:
+        gitignore.write(templates.main)
+
+
+def add_program_template(path, program):
+    if program in templates.programs:
+        with open(path + "/.gitignore", "a") as gitignore:
+            gitignore.write("\n\n")
+            gitignore.write(templates.programs[program])
+    else:
+        print("WARNING: " + program + " not yet supported.")
 
 
 class PathNotFound(Exception):
     pass
 
 
+class ProgramNotFound(Exception):
+    pass
+
+
 if __name__ == "__main__":
     args_number = len(sys.argv)
 
-    # The first argument should be the path
-    path = sys.argv[1]
-    if path == "-h":
+    # Remove program name from argv
+    sys.argv.pop(0)
+
+    # Search for the provided path and check existance
+    if "-h" in sys.argv:
         path = os.getcwd()
-    elif not os.path.exists(sys.argv[1]):
-        msg = "The first argument is not an existing path."
-        raise PathNotFound(msg)
+        sys.argv.remove("-h")
     else:
+        path = sys.argv[0]
+        sys.argv.pop(0)
+        if not os.path.exists(path):
+            msg = "The first argument is not an existing path."
+            raise PathNotFound(msg)
         if path.endswith("/"):
             path = path[:-1]
+
+    # Create/improve .gitignore file
+    gitignore_exists = check_gitignore_existance(path)
+    if gitignore_exists:
+        print(
+            "WARNING: a .gitignore file already exists.\n\n Content will be appended to the existing file."
+        )
+    create_gitignore(path)
+
+    # Add templates from provided programs
+    for program in sys.argv:
+        add_program_template(path, program)
