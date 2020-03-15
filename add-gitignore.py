997c7045a526e9255664721e2f66b6d212f0f346
Damiano <damiano.lodi.93@gmail.com>
2020-03-15 16:20:17 +0100

    Add documentation to functions

--- add-gitignore.py
+++ add-gitignore.py
@@ -4,6 +4,16 @@ import templates
 
 
 def check_gitignore_existance(path):
+    """Check if gitignore already exists.
+
+    Args:
+        path: a string containing the path in which the check
+            should be executed.
+    
+    Returns:
+        True if the .gitignore file already exists.
+        False otherwise.
+    """
     file_list = os.listdir(path)
     if ".gitignore" in file_list:
         return True
@@ -11,17 +21,42 @@ def check_gitignore_existance(path):
 
 
 def create_gitignore(path):
+    """Create the gitignore file.
+
+    Args:
+        path: string containing the path in which the file
+            should be created.
+    """
     with open(path + "/.gitignore", "a") as gitignore:
         gitignore.write(templates.main)
 
 
 def add_program_template(path, program):
+    """Add program templates to the existing gitignore file.
+
+    This function search the provided program inside the 'programs'
+    dictionary of the templates.py file. If the program is found,
+    the associated template will be appended to the existing gitignore
+    file. Otherwise, if the program is not found, a warning message
+    is printed to the user.
+
+    Args:
+        path: string containing the path in which the .gitignore
+            file is present.
+        program: string with the name of the program provided by
+            the user in the arguments.
+
+    Returns:
+        Return 1 if the program is not found inside the programs
+        dictionary. Otherwise, return 0.
+    """
     if program in templates.programs:
         with open(path + "/.gitignore", "a") as gitignore:
             gitignore.write("\n\n")
             gitignore.write(templates.programs[program])
     else:
-        print("WARNING: " + program + " not yet supported.")
+        return 1
+    return 0
 
 
 class PathNotFound(Exception):
@@ -33,12 +68,10 @@ class ProgramNotFound(Exception):
 
 
 if __name__ == "__main__":
-    args_number = len(sys.argv)
-
     # Remove program name from argv
     sys.argv.pop(0)
 
-    # Search for the provided path and check existance
+    # Search for the provided path and check its existance
     if "-h" in sys.argv:
         path = os.getcwd()
         sys.argv.remove("-h")
@@ -61,4 +94,7 @@ if __name__ == "__main__":
 
     # Add templates from provided programs
     for program in sys.argv:
-        add_program_template(path, program)
+        error = add_program_template(path, program)
+        if error:
+            print("WARNING: " + program + " not yet supported.")
+
